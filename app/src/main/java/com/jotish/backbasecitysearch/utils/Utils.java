07095c87c78a6ad310d385f78da0b1c1c0e1a948
jotish <jotish@ymail.com>
2017-06-13 19:46:03 +0530

    fixing crashes

--- app/src/main/java/com/jotish/backbasecitysearch/utils/Utils.java
+++ app/src/main/java/com/jotish/backbasecitysearch/utils/Utils.java
@@ -66,9 +66,13 @@ public class Utils {
     }
   }
   public static void hideKeyboard(Activity activity) {
+    try {
       InputMethodManager inputManager = (InputMethodManager) activity
           .getSystemService(Context.INPUT_METHOD_SERVICE);
       inputManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(),
           InputMethodManager.HIDE_NOT_ALWAYS);
+    } catch (Exception e) {
+      
+    }
   }
 }
